d6c64430ce5d90d2d1f8f68447bc16c5ff4c7a6d
Kostiantyn <konstantinsh579@gmail.com>
2021-06-20 09:22:08 +0300

    revork add fix

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-const slides = document.querySelectorAll('.slide');
+function slidesPlugin(activeSlide = 0) {
+	const slides = document.querySelectorAll('.slide');
 
+	slides[activeSlide].classList.add('active');
+
+	for (const slide of slides) {
+		slide.addEventListener('click', () => {
+			clearActiveClasses();
+
+			slide.classList.add('active');
+		});
+	}
+
+	function clearActiveClasses() {
+		slides.forEach((slide) => {
+			slide.classList.remove('active');
+		});
+	}
+}
+slidesPlugin(1);
 // my firs version
 /* const hideAllCards = () => {
 	slides.forEach((slide) => {
@@ -13,17 +31,3 @@ slides.forEach((slide) => {
 		target.classList.add('active');
 	});
 }); */
-
-for (const slide of slides) {
-	slide.addEventListener('click', () => {
-		clearActiveClasses();
-
-		slide.classList.add('active');
-	});
-}
-
-function clearActiveClasses() {
-	slides.forEach((slide) => {
-		slide.classList.remove('active');
-	});
-}
